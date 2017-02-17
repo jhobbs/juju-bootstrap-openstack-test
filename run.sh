3d798714d76472f6c108cb9afd79bbacc859885d
Jason Hobbs <jason.hobbs@canonical.com>
2017-02-17 21:43:42 +0000

    Make an xunit result.

--- run.sh
+++ run.sh
@@ -10,9 +10,19 @@ export OPENSTACK_MODEL=$1
 export MODEL_NAME=$(echo $OPENSTACK_MODEL | cut -f 2 -d :)
 export NEW_CLOUD=${MODEL_NAME}-openstack
 export NEW_CONTROLLER=$NEW_CLOUD-RegionOne
+ARTIFACTS_FOLDER=$2
+
+mkdir -p $ARTIFACTS_FOLDER
+
 . novarc
 
 ./setup-quotas
 ./bootstrap-juju2
 ./test-ubuntu-deploy
 juju destroy-controller ${NEW_CONTROLLER} --destroy-all-models --yes
+
+cat <<EOF > $ARTIFACTS_FOLDER/juju_bootstrap_xunit.xml
+<testsuite errors="0" failures="0" name="" tests="1">
+<testcase classname="TestJujuBootstrap" name="Test Juju Bootstrap">
+</testsuite>
+EOF
